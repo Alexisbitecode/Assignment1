c83223608c6f24a484b86cb7572a407ad64b7aad
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-09-28 18:58:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns  # Add this import statement
 
-st.subheader("First Graph")
+st.write("""
+In this project, we will study the factors that affect students' reading and math scores, including parental level of education, test preparation course, lunch, gender, and race/ethnicity	
+""")
 
+
+st.subheader("Parental Level of Education")
 # Load the data from a CSV file
 df = pd.read_csv("StudentsPerformance.csv")
 df1 = df.groupby("parental level of education")[["math score", "reading score"]].mean()
-
-
 fig = plt.figure(figsize=(10, 6))
 sns.scatterplot(x="math score", y="reading score", hue="parental level of education", data=df1)
 plt.xlabel("Math Score")
@@ -17,16 +19,40 @@ plt.ylabel("Reading Score")
 plt.title("Scatter Plot of Math Score vs. Reading Score (Colored by Parental Education)")
 # Display the plot in Streamlit
 st.pyplot(fig)
-
 # Make some comments
 st.write("""
 We can see from the graph that students' math and reading scores are positively related to the parental level of education. The higher the parental level of education, the higher grades their kids can get. This quite makes sense since parents play a pretty important role in kids' education.
 """)
 
-st.subheader("Second Table")
 
+st.subheader("Test Preparation Course")
 # Second table
 df2=df.groupby("test preparation course")[["math score", "reading score"]].mean()
 st.write("Test preparation course & Scores:")
 st.write(df2)
 st.write(""" We can see from the table that the average math and reading scores are higher for the students who have completed the test preparation course.""")
+
+
+st.subheader("Lunch")
+df3=df.groupby("lunch")[["math score", "reading score"]].mean()
+data = {
+    'lunch': list(df3.index)*2,
+    'subject': ['math', 'math', 'reading', 'reading'],
+    'score': list(df3['math score'])+list(df3['reading score'])
+}
+df3 = pd.DataFrame(data)
+
+# Streamlit app title
+st.title('Barplot of Math and Reading Scores Colored by Lunch Type')
+
+# Create a barplot of math and reading scores colored by lunch type
+plt.figure(figsize=(8, 6))
+sns.barplot(x="subject", y="score", hue="lunch", data=df3)
+plt.xlabel('Subject')
+plt.ylabel('Score')
+plt.title('Barplot of Math and Reading Scores Colored by Lunch Type')
+plt.legend(title='Lunch Type')
+
+# Display the plot in Streamlit
+st.pyplot(plt)
+st.write(""" From the bar plot, we can see that the average math and reading scores are higher for students who have a standard lunch. This makes quite a bit of sense since when students receive more nutrients, they tend to perform better in their studies.""")
