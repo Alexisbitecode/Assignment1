a65033785424a22d63297710c4300bd5d4680a58
Alexisbitecode <143207524+Alexisbitecode@users.noreply.github.com>
2023-10-03 00:39:06 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,47 +55,23 @@ st.write(""" From the bar plot, we can see that the average math and reading sco
 
 
 #Fourth Graph
-
 # Create a figure with two subplots (one for math scores, one for reading scores)
+st.subheader("Gender")
 fig, axes = plt.subplots(1, 2, figsize=(12, 6))
-
 # Plot the violin plot for math scores
 sns.violinplot(x="gender", y="math score", data=df, palette="Set1", split=True, ax=axes[0])
 axes[0].set_xlabel("Gender")
 axes[0].set_ylabel("Math Score")
 axes[0].set_title("Violin Plot of Math Scores by Gender")
-
 # Plot the violin plot for reading scores
 sns.violinplot(x="gender", y="reading score", data=df, palette="Set2", split=True, ax=axes[1])
 axes[1].set_xlabel("Gender")
 axes[1].set_ylabel("Reading Score")
 axes[1].set_title("Violin Plot of Reading Scores by Gender")
-
 # Adjust layout
 plt.tight_layout()
-
 # Display the plot in Streamlit
 st.pyplot(fig)
-
-
-st.subheader("Gender")
-df4=df.groupby("gender")[["math score", "reading score"]].mean()
-data = {
-    'gender': list(df4.index)*2,
-    'subject': ['math', 'math', 'reading', 'reading'],
-    'score': list(df4['math score'])+list(df4['reading score'])
-}
-df4 = pd.DataFrame(data)
-# Create a barplot of math and reading scores colored by lunch type
-plt.figure(figsize=(8, 6))
-sns.barplot(x="subject", y="score", hue="lunch", data=df3)
-plt.xlabel('Subject')
-plt.ylabel('Score')
-plt.title('Barplot of Math and Reading Scores Colored by Gender')
-plt.legend(title='Gender')
-# Display the plot in Streamlit
-st.pyplot(plt)
-st.write(""" From the bar plot, we can see that the average math and reading scores are higher for students who have a standard lunch. This makes quite a bit of sense since when students receive more nutrients, they tend to perform better in their studies.""")
 
 
 st.subheader("Race/ethnicity")
